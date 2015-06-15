fbc59bb9ca74804bfff2325ce6354b06a90a161c
Jacob Arriola <jacob.arriola@gmail.com>
2015-06-14 20:07:20 -0700

    Add controller

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+(function() {
 var app = angular.module('store',[]);
+
+  app.controller('StoreController', function () {
+    this.product = gem;
+  });
+
+  var gem = {
+    name: 'Dodecahedron',
+    price: 2.95,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  }
+
+})();
